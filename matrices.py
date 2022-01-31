4fd6247a31840c86f16fd9bbd5c436496781a00e
Leo Tyrpak <tyrpakleo@gmail.com>
2022-01-31 19:01:18 +0000

    amending

--- matrices.py
+++ matrices.py
@@ -1,26 +1,16 @@
+import random
+
+from polynomials import Polynomial
+
+
 class Matrix:
     bad = 'Improper'
     bad_dimension = 'Incorrect dimensions'
 
     def __init__(self, array):
-
         self.__coefficients = array
 
-    @staticmethod
-    def constant_matrix(n, m, constant):
-        if n <= 0 or m <= 0:
-            print(Matrix.bad)
-            return
-        return Matrix([[constant for i in range(m)] for j in range(n)])
-
-    @staticmethod
-    def zeros(n, m):
-        return Matrix.constant_matrix(n, m, 0)
-
-    @staticmethod
-    def ones(n, m):
-        return Matrix.constant_matrix(n, m, 1)
-
+    #a bunch of useful properties
     @property
     def coefficients(self):
         return self.__coefficients
@@ -30,6 +20,12 @@ class Matrix:
         self.__coefficients = value
 
     @property
+    def transpose(self):
+        matrix = self.coefficients
+        new = [[matrix[i][j] for i in range(self.rows)] for j in range(self.columns)]
+        return Matrix(new)
+
+    @property
     def rows(self):
         return len(self.coefficients)
 
@@ -37,13 +33,18 @@ class Matrix:
     def columns(self):
         return len(self.coefficients[0])
 
-    def set_coefficient(self, value, row, column):
+    @property
+    def is_square(self):
+        return self.columns == self.rows
+
+    #useful functions to simplify computation
+    def set_coefficient(self, row, column, value):
         if self.check_coefficient(row, column):
             self.__coefficients[row][column] = value
 
     def change_coefficient(self, value, row, column):
         if self.check_coefficient(row, column):
-            self.__coefficients[row][column] += value
+            self.set_coefficient(row, column, self.get_coefficient(row, column) + value)
 
     def get_coefficient(self, row, column):
         if self.check_coefficient(row, column):
@@ -58,13 +59,34 @@ class Matrix:
     def multiply_constant(self, constant):
         matrix = self.coefficients
         new = [[constant * matrix[i][j] for j in range(self.columns)] for i in range(self.rows)]
-        self.coefficients = new
-
-    def multiply_constant(self, constant):
-        matrix = self.coefficients
-        new = [[constant * matrix[i][j] for j in range(self.columns)] for i in range(self.rows)]
         return Matrix(new)
 
+    #generate generic matrices
+    @staticmethod
+    def constant(n, m, constant):
+        if n <= 0 or m <= 0:
+            print(Matrix.bad)
+            return
+        return Matrix([[constant for i in range(m)] for j in range(n)])
+
+    @staticmethod
+    def zeros(n, m):
+        return Matrix.constant(n, m, 0)
+
+    @staticmethod
+    def ones(n, m):
+        return Matrix.constant(n, m, 1)
+
+    #generate random matrices with coefficients in a uniform distribution
+    @staticmethod
+    def generate_random(n, m, big):
+        return Matrix([[random.random() * big for j in range(m)] for i in range(n)])
+
+    @staticmethod
+    def generate_random_uniform(n, m):
+        return Matrix.generate_random(n, m, 1)
+
+    #overrides the basic operations for matrices
     def __add__(self, operand):
         if not self.check_dimensions(operand):
             print(Matrix.bad_dimension)
@@ -76,12 +98,6 @@ class Matrix:
 
     def __sub__(self, operand):
         return self + Matrix.multiply_constant(operand, -1)
-
-    @property
-    def transpose(self):
-        matrix = self.coefficients
-        new = [[matrix[i][j] for i in range(self.rows)] for j in range(self.columns)]
-        return Matrix(new)
 
     def __mul__(self, operand):
         if self.columns != operand.rows:
@@ -104,6 +120,10 @@ class Matrix:
             string += ']\n'
         return string
 
+    def __copy__(self):
+        return Matrix(self.coefficients.copy())
+
+    #useful methods for computation
     @staticmethod
     def delete_column_list(matrix, n):
         if n < 0 or n >= len(matrix[0]):
@@ -131,55 +151,124 @@ class Matrix:
     def delete_row(operand, n):
         return Matrix(Matrix.delete_row_list(operand.coefficients, n))
 
+#a special class for square matrices with extra functionality
+class SquareMatrix(Matrix):
+
+    #sets up array then calculates characteristic poly
+    def __init__(self, array):
+        super().__init__(array)
+        self.__characteristic = SquareMatrix.characteristic_helper(array)
+
+    @property
+    def characteristic(self):
+        return self.__characteristic
+
+    #overrides columns property since square dimension to make it easier
+    @property
+    def columns(self):
+        return self.rows
+    #returns sum of diagonal entries, the trace
+    @property
+    def trace(self):
+        return sum([self.get_coefficient(i, i) for i in range(self.rows)])
+
+    #evaluates determinant as characteristic poly at 0
     @property
     def determinant(self):
-        if self.columns != self.rows:
-            print(Matrix.bad_dimension)
-            return
-        return Matrix.determinant_helper(self.coefficients)
+        return self.characteristic.evaluate(0)
 
+    #calculates the determinant for a matrix containing poly values
     @staticmethod
     def determinant_helper(matrix):
         n = len(matrix)
         if n == 1:
             return matrix[0][0]
         reduced = Matrix.delete_row_list(matrix, 0)
-        return sum([matrix[0][j] * (-1) ** j * Matrix.determinant_helper(Matrix.delete_column_list(reduced, j)) for j in
-                    range(n)])
-    def power(self, n):
-        if n < 0 or self.columns!=self.rows:
-            return
-        return self.power_helper(n)
+        coeffs = [matrix[0][j] * Polynomial.constant((-1) ** j) for j in range(n)]
+        smaller = [SquareMatrix.determinant_helper(Matrix.delete_column_list(reduced, j)) for j in range(n)]
+        initial = Polynomial()
+        for j in range(n):
+            initial += coeffs[j] * smaller[j]
+        return initial
 
-    def power_helper(self, n):
+    #takes a matrix and calculates the characteristic poly by turning it into a matrix of polynomials
+    @staticmethod
+    def characteristic_helper(matrix):
+        n = len(matrix)
+        polynomials = [[Polynomial.constant(matrix[i][j]) for j in range(n)] for i in range(n)]
+        for i in range(n):
+            polynomials[i][i] -= Polynomial.get_x()
+        return SquareMatrix.determinant_helper(polynomials)
+
+    #creates generic matrix of certain size with constant values
+    @staticmethod
+    def constant_matrix(n, constant):
+        if n <= 0:
+            print(Matrix.bad)
+            return
+        return SquareMatrix([[constant for i in range(n)] for j in range(n)])
+
+    @staticmethod
+    def zero(n):
+        return SquareMatrix.constant_matrix(n, 0)
+
+    @staticmethod
+    def ones(n):
+        return SquareMatrix.constant_matrix(n, 1)
+
+    @staticmethod
+    def identity(n):
+        return SquareMatrix.identity_constant(n, 1)
+
+    @staticmethod
+    def identity_constant(n, constant):
+        new = SquareMatrix.zero(n)
+        for i in range(n):
+            new.set_coefficient(n, n, constant)
+        return new
+
+    #generates matrices with coefficients in a uniform distribution
+    @staticmethod
+    def generate_random(n, range):
+        return SquareMatrix([[random.random() * range for j in range(n)] for i in range(n)])
+
+    @staticmethod
+    def generate_random_uniform(n):
+        return Matrix.generate_random(n, n, 1)
+
+    #overrides methods
+    def __pow__(self, n):
+        if n < 0:
+            return
         if n == 0:
-            return Matrix(Matrix.ones(n,n))
+            return SquareMatrix.identity(self.rows)
         if n == 1:
             return self.__copy__()
-        return self * self.power_helper(n - 1)
-    def __copy__(self):
-        return Matrix(self.coefficients.copy())
+        return self * self ** (n - 1)
+
 
 
 def main():
     test = [[1, 2, 3], [3, 4, 5], [9, 6, 0]]
     matrix1 = Matrix.ones(2, 9)
     matrix2 = Matrix.ones(2, 9)
-    matrix3 = Matrix(test)
-    print(matrix1+matrix2)
+    matrix3 = SquareMatrix(test)
+    # print(matrix1 + matrix2)
     print(matrix3)
-    print(matrix3.determinant)
-    print(matrix1)
-    print(matrix1.transpose)
-    print(Matrix.delete_row(matrix1, 1))
-    print(Matrix.delete_column(matrix1, 1))
-    print(matrix1)
-    print(matrix1.columns)
-    # print(Matrix.multiply_matrices(matrix1, matrix1.transpose))
-    # matrix1.add(matrix2)
+    print(SquareMatrix.generate_random_uniform(4))
+    print(matrix3.characteristic)
+    print(matrix3 ** 2)
+    print(matrix3)
     # print(matrix1)
-    print(matrix3.power(3))
-    print(matrix3)
+    # print(matrix1.transpose)
+    # print(Matrix.delete_row(matrix1, 1))
+    # print(Matrix.delete_column(matrix1, 1))
+    # print(matrix1)
+    # print(matrix1.columns)
+    # print(matrix1*matrix1.transpose))
+    # print(matrix1)
+    # print(matrix3**3)
+    # print(matrix3)
 
 
 main()
--- polynomials.py
+++ polynomials.py
@@ -11,6 +11,7 @@ class Polynomial:
         self.__coefficients = coeffs
         self.check_zeros()
 
+    # useful properties and setters
     @property
     def coefficients(self):
         return self.__coefficients
@@ -26,8 +27,16 @@ class Polynomial:
 
     @property
     def leading_coefficient(self):
-        return self.coefficients[-1]
+        return self.get_coefficient(self.degree - 1)
 
+    @property
+    def derivative(self):
+        if self.degree <= 1:
+            return Polynomial.zero()
+        else:
+            return Polynomial([(i + 1) * self.get_coefficient(i + 1) for i in range(self.degree - 1)])
+
+    # generates generic polynomials of certain shape
     @staticmethod
     def get_power_lead(n, lead):
         new = [0 for i in range(n)]
@@ -39,43 +48,24 @@ class Polynomial:
         return Polynomial.get_power_lead(n, 1)
 
     @staticmethod
-    def get_one():
-        return Polynomial.get_power(0, 1)
+    def constant(constant):
+        return Polynomial.get_power_lead(0, constant)
+
+    # useful polynomials that are regularly used
+    @staticmethod
+    def identity():
+        return Polynomial([1])
+
+    @staticmethod
+    def zero():
+        return Polynomial([0])
 
     @staticmethod
     def get_x():
-        return Polynomial.get_power(1, 1)
+        return Polynomial.get_power(1)
 
-    def power(self, n):
-        if n < 0:
-            return
-        return self.power_helper(n)
-
-    def power_helper(self, n):
-        if n == 0:
-            return Polynomial([1])
-        if n == 1:
-            return self.__copy__()
-        return self * self.power_helper(n - 1)
-
-    def __str__(self):
-        string = 'f(x) = '
-        for i in range(self.degree):
-            value = self.coefficients[i]
-            if value != 0:
-                string += str(value)
-                if i > 0:
-                    string += 'x'
-                if i > 1:
-                    string += '^' + str(i)
-                string += ' + '
-
-        return string[:len(string) - 2]
-
-    def __copy__(self):
-        return Polynomial(self.coefficients.copy())
-
-    def __set_coefficients(self, index, number):
+    #manipulation of polynomials
+    def set_coefficients(self, index, number):
         if index < 0:
             print('not possible')
         if index < self.degree:
@@ -89,7 +79,7 @@ class Polynomial:
 
     def change_coefficient(self, index, number):
         current = self.get_coefficient(index)
-        self.__set_coefficients(index, number + current)
+        self.set_coefficients(index, number + current)
 
     def get_coefficient(self, index):
         if index < 0:
@@ -104,10 +94,18 @@ class Polynomial:
             return
         i = self.degree - 1
         while self.coefficients[i] == 0:
+            if i == -1:
+                self.coefficients = []
+                return
             i -= 1
         if i != self.degree - 1:
             self.coefficients = self.coefficients[:i + 1]
 
+    #returns polynomial evaluated at a value
+    def evaluate(self, number):
+        return sum([self.get_coefficient(i) * number ** i for i in range(self.degree)])
+
+    # overrides methods
     def __add__(self, operand):
         n = max(self.degree, operand.degree)
         coeffs = [self.get_coefficient(i) + operand.get_coefficient(i) for i in range(n)]
@@ -118,18 +116,12 @@ class Polynomial:
         coeffs = [self.get_coefficient(i) - operand.get_coefficient(i) for i in range(n)]
         return Polynomial(coeffs)
 
-    def multiply_constant(self, constant):
-        return Polynomial([i * constant for i in self.coefficients])
-
     def __mul__(self, operand):
         new = [0 for i in range(self.degree + operand.degree - 1)]
         for i in range(self.degree):
             for j in range(operand.degree):
                 new[i + j] += self.get_coefficient(i) * operand.get_coefficient(j)
         return Polynomial(new)
-
-    def evaluate(self, number):
-        return sum([self.coefficients[i] * number ** i for i in range(self.degree)])
 
     def __mod__(self, operand):
         if operand.degree == 0:
@@ -141,15 +133,90 @@ class Polynomial:
         subtract = operand * Polynomial.get_power_lead(d, self.leading_coefficient / operand.leading_coefficient)
         return (self - subtract) % operand
 
+    def __pow__(self, n):
+        if n < 0:
+            return
+        if n == 0:
+            return Polynomial.identity()
+        if n == 1:
+            return self.__copy__()
+        return self * self ** (n - 1)
+
+    def __str__(self):
+        string = 'f(x) = '
+        for i in range(self.degree):
+            value = self.coefficients[i]
+            if value != 0:
+                if i != 0 and value == -1:
+                    string += '-'
+                elif i == 0 or value != 1:
+                    string += str(value)
+                if i > 0:
+                    string += 'x'
+                if i > 1:
+                    string += '^' + str(i)
+                string += ' + '
+
+        return string[:len(string) - 2]
+
+    def __copy__(self):
+        return Polynomial(self.coefficients.copy())
+
+
+
+    # interpolation methods
+    @staticmethod
+    def interpolation(values, interpolants):
+        n = len(values)
+        if n != len(interpolants):
+            print('Bad dimension')
+            return
+        new = Polynomial()
+        for i in range(n):
+            current = Polynomial.constant(interpolants[i])
+            for j in range(n):
+                if j != i:
+                    pol = Polynomial.get_x() - Polynomial.constant(interpolants[j])
+                    current *= pol * Polynomial.constant(1 / (values[i] - values[j]))
+            new += current
+        return new
+
+    @staticmethod
+    def hermite_interpolation(values, interpolants, derivatives):
+        n = len(values)
+        if n != len(interpolants) or n != len(derivatives):
+            print('Bad dimension')
+            return
+        new = Polynomial()
+        for i in range(n):
+            current = Polynomial.identity()
+            for j in range(n):
+                if j != i:
+                    pol = Polynomial.get_x() - Polynomial.constant(interpolants[j])
+                    current *= pol * Polynomial.constant(1 / (values[i] - values[j]))
+            square = current * current
+            difference = Polynomial.get_x() - Polynomial.constant(values[i])
+            H = square * (Polynomial.identity() - Polynomial.constant(
+                2 * current.derivative.evaluate(values[i])) * difference)
+            K = square * difference
+            new += H * Polynomial.constant(interpolants[i]) + K * Polynomial.constant(derivatives[i])
+        return new
+
 
 def main():
     coeffs1 = [6, 1, 2, 6, 4]
     coeffs2 = [2, 3, 4]
+    values = [0, 1]
+    interpolants = [2, 3]
+    derivatives = [8, 9]
+    print(Polynomial.hermite_interpolation(values, interpolants, derivatives))
     pol1 = Polynomial(coeffs1)
     pol2 = Polynomial(coeffs2)
+    print(pol2)
+    print(pol2.derivative)
     print(pol2.leading_coefficient)
     print(pol1)
-    print(pol1.multiply_constant(2))
+    print(pol1 * Polynomial.constant(2))
     print(pol1.evaluate(1))
     print(pol2)
     pol3 = pol1 * pol2
@@ -159,7 +226,6 @@ def main():
     print(pol1 % Polynomial.get_power(2))
     print(pol3.leading_coefficient)
     print(pol3.coefficients)
-    print(pol3.power(3))
-
+    print(pol3 ** 3)
 
 main()
