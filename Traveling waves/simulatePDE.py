47c6e349c70d6ea30ab6ddd723c4dd444b32c000
Leo Tyrpak <tyrpakleo@gmail.com>
2024-10-28 11:14:13 +0000

    amending

--- Traveling waves/simulatePDE.py
+++ Traveling waves/simulatePDE.py
@@ -2,14 +2,30 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import scipy
+from numpy.f2py.auxfuncs import throw_error
+
+
+# This function gets the input from the user
+def get_typed_input(prompt, expected_type=int, allowed_values=None):
+    while True:
+        try:
+            user_input = expected_type(input(prompt))
+            if allowed_values != None and user_input not in allowed_values:
+                raise ValueError('Number outside of range')
+            return user_input
+        except ValueError as e:
+            print(f"Erreur : {e}.")
+
+
 
 # model details
 # initial conditions
 a = 1
 mu = 1
-dimension = 1
-T = 1  # total time
-L = 25.0  # How far it extends in space
+dimension = get_typed_input("What should be the dimension of the simulation(either 1 or 2)?",
+                            allowed_values=[1,2])  # dimension of the space
+T = get_typed_input("How long should the simulation run for(recommended is 20)?")  # total time
+L = get_typed_input("How big should the simulation be(recommended is 25)?")  # How far it extends in space
 
 variance_init = 1.5  # Variance of initial distribution
 sd_limit_init = 4  # How far we compute the initial configuration away in standard deviations
@@ -23,8 +39,10 @@ f_init = lambda x: f(x, variance_init)
 f_kernel = lambda x: f(x, variance_kernel)
 
 # accuracy of approximation+ bounds
-dx = .1  # space step
-dt = .001  # time step
+dx = get_typed_input("What should the dx parameter be(good choice is .1)?",
+                     expected_type=float)   # space step
+dt = get_typed_input("What should the dt parameter be(good choice is .001)?",
+                     expected_type=float)  # time step
 
 # scaling
 f_init_scaled = lambda x: f_init([z * dx for z in x])
@@ -49,22 +67,39 @@ step_plot = iterations // number_plots
 class one_dimensional():
     f_init_scaled = lambda x: dx * f_init([z * dx for z in x])
     f_kernel_scaled = lambda x: dx * f_kernel([z * dx for z in x])
-
+    def __init__(self,k=1,dirac=False,diameter_grid=diameter_grid,time=0,
+                 initial_condition=None):
+        self.matrices=np.zeros((k,diameter_grid))
+        self.laplacian=np.zeros(diameter_grid-2)
+        self.dirac=dirac
+        self.time=time
+        self.conv_matrix=np.array(one_dimensional.convolution_matrix(g=
+                            one_dimensional.f_kernel_scaled,size=radius_kernel_scaled))
+        if initial_condition!=None:
+            self.matrix=initial_condition
+    @staticmethod
     def convolution_matrix(g, size):
         return [g([i - size]) for i in range(2 * size + 1)]
 
-    def laplacian(Z):
-        Ztop = Z[0:-2]
-        Zbottom = Z[2:]
-        Zcenter = Z[1:-1]
-        return Ztop + Zbottom - 2 * Zcenter
+    def update_laplacian(self):
+        Ztop = self.matrix[0:-2]
+        Zbottom = self.matrix[2:]
+        Zcenter = self.matrix[1:-1]
+        self.laplacian= Ztop + Zbottom - 2 * Zcenter
+    @staticmethod
+    def laplacian(V):
+        Ztop = V[0:-2]
+        Zbottom = V[2:]
+        Zcenter = V[1:-1]
+        return  Ztop + Zbottom - 2 * Zcenter
 
-    def convolution(V, conv_matrix, dirac=False):
-        if not dirac:
-            return scipy.ndimage.convolve1d(V, weights=conv_matrix, mode='reflect')
-        return np.array(V)
+    def convolution(self):
+        if not self.dirac:
+            return scipy.ndimage.convolve1d(self.matrix, weights=self.conv_matrix, mode='reflect')
+        return np.array(self.matrix)
 
     # This returns a plot of the graph of U
+    @staticmethod
     def show_patterns(U, ax=None, axis_off=True):
         ax.plot(U)
         if axis_off:
@@ -85,12 +120,12 @@ class one_dimensional():
         V = np.zeros(diameter_grid)
         V[min_point:max_point] += np.ones(max_point - min_point)
         return V
-
-    def update_explicit_euler_equation(t, V, conv_matrix, dirac):
-        return one_dimensional.update_explicit_euler_k_equation(t, V, conv_matrix, dirac)
-
     # We use Runge Kutta 4 to solve the autonomous system
-    def update_explicit_euler_k_equation(t, V, conv_matrix, dirac):
+    def update(self):
+        t=self.time
+        V=self.matrix
+        dirac=self.dirac
+        conv_matrix=self.conv_matrix
         k_1 = one_dimensional.f(t, V, conv_matrix, dirac)
         k_2 = one_dimensional.f(t + dt / 2, V + dt / 2 * k_1, conv_matrix, dirac)
         k_3 = one_dimensional.f(t + dt / 2, V + dt / 2 * k_2, conv_matrix, dirac)
@@ -98,9 +133,11 @@ class one_dimensional():
         W = V + dt / 6 * (k_1 + 2 * k_2 + 2 * k_3 + k_4)
         W[:, 0] = W[:, 1]
         W[:, -1] = W[:, -2]
-        return W
+        self.time+=dt
+        self.matrix=W
 
     # we consider autonomous ODE y'(t)=f(t,y), gives back vector f(t,y)
+    @staticmethod
     def f(t, V, conv_matrix, dirac):
         sumV = [sum([V[j, i] for j in range(len(V[:, 0]))]) for i in range(len(V[0, :]))]
         conv = one_dimensional.convolution(sumV, conv_matrix, dirac)
@@ -246,11 +283,11 @@ def main2(k, dirac):
                 U[j, :] = np.append(U[j, 1:], 0)
 
         U = one_dimensional.update_explicit_euler_k_equation(i * dt, U, conv_matrix, dirac)
-    fig, ax=plt.subplots(1,1,figsize=(8,8))
-    one_dimensional.show_patterns(U, ax=ax)
     plt.show()
     return
 
+if dimension==1:
+    main2(k=10, dirac=False)
+elif dimension==2:
+    main1()
 
-# main1()
-main2(k=6, dirac=False)
