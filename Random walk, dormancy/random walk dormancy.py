d609f0070018eaeea71d926e72fd28855349d659
tyrpakleo <tyrpakleo@gmail.com>
2025-01-29 16:39:10 +0000

    amending

--- Random walk, dormancy/random walk dormancy.py
+++ Random walk, dormancy/random walk dormancy.py
@@ -1,22 +1,20 @@
+from tqdm import tqdm
 import time
 
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 # Random walk simulation
 #x_0, y_0, a_0 are the initial coordinates and state of the walker
 #t is the time elapsed
 
-# Paramètres par défaut
-s_1 = 1
-s_0 = 1
-kappa = 1
-rho = 1
-gamma = 1
 
-def simulate_walk_end_time(t, d, x_0=None, a_0=0, x_1=None, a_1=1):
-    return len(simulate_walk_hits(t, d, -1, x_0, a_0, x_1, a_1))
-def simulate_walk_hits(t_max, d, max_hits, x_0=None, a_0=0, x_1=None, a_1=1):
-    """Simule une marche aléatoire avec dormance jusqu'à ce qu'un certain nombre de hits soit atteint ou que le temps maximum soit dépassé.
+
+def simulate_walk_end_time(t, d, x_0=None, a_0=0, x_1=None, a_1=1,s_1=1,s_0=1,kappa=1,rho=1):
+    return len(simulate_walk_hits(t, d, -1, x_0, a_0, x_1, a_1,s_1,s_0,kappa,rho))
+def simulate_walk_hits(t_max, d, max_hits, x_0=None, a_0=0, x_1=None, a_1=1,s_1=1,s_0=1,kappa=1,rho=1):
+    """Simule une marche aléatoire avec dormance jusqu'à ce qu'un certain nombre de hits soit atteint
+    ou que le temps maximum soit dépassé.
     Retourne une liste des temps entre les hits successifs."""
     ignore=False
     if x_0 is None:
@@ -71,30 +69,83 @@ def simulate_walk_hits(t_max, d, max_hits, x_0=None, a_0=0, x_1=None, a_1=1):
             old_hit=time_elapsed
     return hit_times
 
-def simulate_different_times(end_times,repetitions, d, x_0=None, a_0=0, x_1=None, a_1=1):
+def simulate_different_times(end_times,repetitions, d, x_0=None, a_0=0, x_1=None, a_1=1,s_1=1,s_0=1,kappa=1,rho=1):
     hits_different_times = np.zeros((len(end_times), repetitions))
-    for i in range(len(end_times)):
-        for j in range(repetitions):
-            hits_different_times[i, j] = simulate_walk_end_time(end_times[i], d)
+    total_iterations = len(end_times) * repetitions
+    with tqdm(total=total_iterations, desc="Progress") as pbar:
+        for i in range(len(end_times)):
+            for j in range(repetitions):
+                hits_different_times[i, j] = simulate_walk_end_time(end_times[i], d, x_0, a_0, x_1, a_1,s_1,s_0,kappa,rho)
+                pbar.update(1)
     return hits_different_times
+def simulate_time(end_time,repetitions, d, x_0=None, a_0=0, x_1=None, a_1=1,s_1=1,s_0=1,kappa=1,rho=1):
+    return simulate_different_times([end_time],repetitions, d, x_0, a_0, x_1, a_1,s_1, s_0, kappa, rho)[0,:]
 
 def __main__():
-    times = [10, 100, 1000, 10000, 100000]
-    repetitions = 1000
-    for i in range(3):
-        d = i + 1  # Dimension
+    # Paramètres par défaut
+    s_1 = 1
+    s_0 = 1
+    kappa = 1
+    rho = 1
+    gamma = 1
+    Lambda = -1
+    exponent = Lambda / (Lambda - gamma)
 
-        start_time = time.time()  # Démarrer le chronomètre
-        hits_different_times = simulate_different_times(times, repetitions, d)
-        end_time = time.time()  # Arrêter le chronomètre
+    max_time=100000
+    repetitions = 100
+    d = int(input("Veuillez entrer la dimension d: "))  # Demander à l'utilisateur la dimension
+    points = np.arange(0, 10.01, 0.1)
+    y=np.zeros(len(points))
+    z=np.zeros(len(points))
+    start_time = time.time()  # Démarrer le chronomètre
+    for point in points:
+        hits_different_times = simulate_time(max_time, repetitions, d, [0] * d, 0, [0] * d, 1, point, s_0, kappa, rho)
+        y[int(point*10)]=np.mean(hits_different_times)
+        z[int(point*10)]=np.mean(np.power(exponent, hits_different_times))
+        """print("En", d, "dimensions:")
+        print("Pour le temps t=", max_time, ", le nombre attendu de hits de (0,0,1) est",
+              np.mean(hits_different_times[:]), "avec un écart-type de", np.std(hits_different_times[:]))
+        feynman_kac = np.power(exponent, hits_different_times)
         print("En", d, "dimensions:")
-        for i in range(len(times)):
-            print("Pour le temps t=", times[i], ", le nombre attendu de hits de (0,0,1) est",
-                  np.mean(hits_different_times[i, :]), "avec un écart-type de", np.std(hits_different_times[i, :]))
+        print("Pour le temps t=", max_time, ", la formule de Feynman-Kac donne", np.mean(feynman_kac[:]),
+              "avec un écart-type de", np.std(feynman_kac[:]))"""
 
-        print("Le code a pris", end_time - start_time, "secondes pour s'exécuter.")
 
-    
+    end_time = time.time()  # Arrêter le chronomètre
+    print("Le code a pris", end_time - start_time, "secondes pour s'exécuter.")
+
+    plt.plot(points, y, label='times hit (0,0,1)')
+
+    # Ajouter des labels et un titre
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Graphique de hit de (0,0,1) en fonction de s_0')
+
+    # Ajouter une légende
+    plt.legend()
+
+    # Afficher le graphique
+    plt.show()
+    plt.plot(points, z, label='FK')
+
+    # Ajouter des labels et un titre
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Graphique de FK formula en fonction de s_0')
+
+    # Ajouter une légende
+    plt.legend()
+
+    # Afficher le graphique
+    plt.show()
+
+    # Vérifier que les tableaux ont la même taille
+    if len(points) == len(y) == len(z):
+        with open('output.txt', 'w') as f:
+            for a, b, c in zip(points, y, z):
+                f.write(f"{a}\t{b}\t{c}\n")
+    else:
+        print("Les tableaux n'ont pas la même taille.")
     """t_max=10000000
     d=1
     max_hits=3000
